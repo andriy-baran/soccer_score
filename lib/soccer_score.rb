dda2262d55af8e0dc3a3b6117e67cc1020e96af4
Andrii Baran <andriy.baran.v@gmail.com>
2018-10-03 16:11:21 +0300

    Added logic to transform game result into ranking points

--- lib/soccer_score.rb
+++ lib/soccer_score.rb
@@ -1,7 +1,14 @@
 require "soccer_score/version"
+require "soccer_score/game_result"
 
 module SoccerScore
   def self.rank(input)
-
+    lines = input.split("\n")
+    table = Hash.new { |hash, key| key = 0 }
+    lines.each do |line|
+      game_result = GameResult.new(line)
+      table[game_result.home_team] += game_result.home_team_points
+      table[game_result.away_team] += game_result.away_team_points
+    end
   end
 end
